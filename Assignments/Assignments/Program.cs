d62fd641b8f47ca4bf862d2f441c62e7e57f1922
Cameron Shelton <camshe96@gmail.com>
2021-03-05 13:24:33 -0700

    This is my first assignment

---
+++ Assignments/Assignments/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anothervariable = MyMethod();
+
+            Console.WriteLine(anothervariable);
+        }
+
+        public static int MyMethod()
+        {
+            int var2 = 12345;
+
+            return var2;
+        }
+    }
+}
